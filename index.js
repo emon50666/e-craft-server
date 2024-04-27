13be742d5fb4b51628078052f73a8362066fdb43
Emon <emon50666@gmail.com>
2024-04-27 20:04:29 +0600

    add .env file and protect mongodb username and password

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 
 
@@ -10,14 +11,11 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// mzIre3zRCp1Woxlf
-// ecarft
 
 
 
-
-const uri = "mongodb+srv://ecarft:mzIre3zRCp1Woxlf@cluster0.mbvqn67.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mbvqn67.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
